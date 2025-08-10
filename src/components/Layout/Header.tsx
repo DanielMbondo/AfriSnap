f9f398d1357f54dc50359fc778ab3f08ff26e1cb
DanielMbondo <danielmbondo2002@gmail.com>
2025-08-10 06:57:46 +0100

    Updated Header.tsx

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
   title?: string;
 }
 
-export const Header: React.FC<HeaderProps> = ({ title = 'Honcho' }) => {
+export const Header: React.FC<HeaderProps> = ({ title = 'AfriSnap' }) => {
   const { user, logout } = useAuth();
 
   return (
